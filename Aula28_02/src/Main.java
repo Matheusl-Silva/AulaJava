4e4a3c0e81bbc390dba7f186a3389d31fe1ca275
Matheus Leandro <matleandrosilva@gmail.com>
2025-02-28 21:43:33 -0300

    ⭐feat: Javinha
    Primeiro em java

---
+++ Aula28_02/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = 10;
+        System.out.println("O primeiro Hello World do Matheus!");
+        System.out.println(numero);
+    }
+}
